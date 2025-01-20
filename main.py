edf435f2483dac5a306d938acca712406b2fe4c8
rodrigueznCDM <rodriguezn@educ.college-montreal.qc.ca>
2025-01-20 14:51:10 -0500

    Période 1

--- main.py
+++ main.py
@@ -11,38 +11,35 @@ SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Mercury"
 
 
-def generate_dust(x_min, x_max, y_min, y_max, color):
-    for i in range(10):
-        radius = random.randint(5, 15)
-
-        x = random.randint(x_min, x_max)
-        y = random.randint(y_min, y_max)
-
-        arcade.draw_circle_filled(x, y, radius, color)
-
-
-def center_dust():
-    generate_dust(350, 450, 50, 200, arcade.color.ASH_GREY)
-
-
-def dark_center_dust():
-    for i in range(10):
-        radius = random.randint(5, 15)
-
-        x = random.randint(350, 450)
-        y = random.randint(50, 200)
-
-        arcade.draw_circle_filled(x, y, radius, arcade.color.ASH_GREY)
-
-
 class MyGame(arcade.Window):
     """
     La classe principale de l'application
     """
-
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.LIGHT_SKY_BLUE)
+
+    @staticmethod
+    def generate_circles(num, x_min, x_max, y_min, y_max, color):
+        for i in range(num):
+            radius = random.randint(5, 10)
+
+            x = random.randint(x_min, x_max)
+            y = random.randint(y_min, y_max)
+
+            arcade.draw_circle_filled(x, y, radius, color)
+
+    def center_dust(self):
+        self.generate_circles(24, 350, 450, 60, 190, arcade.color.ASH_GREY)
+
+    def dark_center_dust(self):
+        self.generate_circles(10, 380, 420, 60, 190, arcade.color.TROLLEY_GREY)
+
+    def engine_fire_yellow(self):
+        self.generate_circles(12, 350, 450, 140, 240, arcade.color.SCHOOL_BUS_YELLOW)
+
+    def engine_fire_orange(self):
+        self.generate_circles(8, 350, 450, 180, 230, arcade.color.BURNT_ORANGE)
 
     def on_draw(self):
         """
@@ -51,7 +48,10 @@ class MyGame(arcade.Window):
         self.clear()
 
         arcade.draw_lrbt_rectangle_filled(0, 800, 0, 50, arcade.color.BATTLESHIP_GREY)
-        center_dust()
+        self.center_dust()
+        self.dark_center_dust()
+        self.engine_fire_yellow()
+        self.engine_fire_orange()
 
     def on_update(self, delta_time):
         """
